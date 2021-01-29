16ba8fcee142f13d03981623e687217b71946123
Nataliya Pogodina <n.pogodina@gmail.com>
2021-01-29 08:59:10 -0800

    Added Entity Framework

---
+++ persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace persistence
+{
+    public class DataContext : DbContext
+    {
+       public DataContext(DbContextOptions options) : base(options)
+       {
+       } 
+
+       public DbSet<Value> Values { get; set; }
+    }
+}
